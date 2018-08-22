4176a4738aafbbf493b4f88879377fb6f166aa23
xannder <arodriguezsantos@student.fullsail.edu>
2018-08-21 20:41:48 -0400

    Update SwapName.cs
    Obtain user first name and last name and swap the username (last name, first name).

--- SwapName.cs
+++ SwapName.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,7 +11,7 @@ namespace DVP1.CE1
         //NAME: Alexander Rodriguez
         //DATE: 1808
         //COURSE: Project & Porfolio 1
-        //SYNOPSIS:
+        //SYNOPSIS: Obtain user first name and last name and swap the username (last name, first name)
 
         /*
         public string firstNam;
@@ -32,18 +32,44 @@ namespace DVP1.CE1
 
             //Clear Console and Welcome user to Swap Name
             Console.Clear();
-            Console.WriteLine("Welcome to SwapName!"); 
+            Console.WriteLine("SWAPNAME");
             Console.WriteLine("********************************************");
             Console.WriteLine("********************************************");
             Console.WriteLine("");
 
-            //Ask user to enter first name
+            Console.WriteLine("Welcome to Swap Name!");
+            Console.WriteLine("");
+
+            //Ask user to enter firstname
             Console.WriteLine("Please enter your first name:");
             firstNam = Console.ReadLine();
+            Console.WriteLine("");
+
+            //Validate the user firstname is not null
+            while(string.IsNullOrWhiteSpace(firstNam))
+            {
+                //Notify user about the null value (firstname)
+                Console.WriteLine("Your entry is null! Please enter your first name:");
+
+                //Capture the user first name
+                firstNam = Console.ReadLine();
+                Console.WriteLine("");
+            }
 
             //Ask user to enter last name
             Console.WriteLine("Thank you {0}, please enter your last name:", firstNam);
             lastNam = Console.ReadLine();
+
+            //Validate the user lastname is not null
+            while (string.IsNullOrWhiteSpace(lastNam))
+            {
+                //Notify user about the null value (lastname)
+                Console.WriteLine("Your entry is null! Please enter your lastname:");
+
+                //Capture the user first name
+                lastNam = Console.ReadLine();
+                Console.WriteLine("");
+            }
 
             //Show the result
             Console.WriteLine("Excellent! Your name ({0} {1}), reversed name would be {1}, {0}", firstNam, lastNam);
