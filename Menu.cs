1a6b087458dc2d5850c6b1a5aeb35fe3c448a776
xannder <arodriguezsantos@student.fullsail.edu>
2018-08-21 20:39:24 -0400

    Update Menu.cs
    Menu that allows the user to select an option to run programs from the coding challenges

--- Menu.cs
+++ Menu.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,20 +11,20 @@ namespace DVP1.CE1
         //NAME: Alexander Rodriguez
         //DATE: 1808
         //COURSE: Project & Porfolio 1
-        //SYNOPSIS: Menu that allow user to select an option to run programs from the coding challanges
+        //SYNOPSIS: Menu that allows the user to select an option to run programs from the coding challenges
 
         //Create fucntion for Menu
 
         public static void ChMenu()
         {
 
-            //Variable for User Selection
+            //Variable for user selection
             string strUserSel;
             int userSel = 0;
             bool valOpt = false;
 
+            //Welcome and header
             Console.WriteLine("");
-
             Console.WriteLine("Welcome to my Project Porfolio Coding Challenge");
             Console.WriteLine("");
 
@@ -33,6 +33,7 @@ namespace DVP1.CE1
             Console.WriteLine("********************************************");
             Console.WriteLine("");
 
+            //Show user menu
             Console.WriteLine("Coding Challenge Menu: \r\nPlease enter the number for the challenge you want to run...");
             Console.WriteLine("");
 
@@ -90,27 +91,41 @@ namespace DVP1.CE1
 
             //Run Menu option depending on user selection
 
+            //If selection is equal to 1 then run SwapName
             if (userSel == 1) { 
                 SwapName.ChSwapName();
-
             }
 
+            //If selection is equal to 2 then run Backwards
             if (userSel == 2)
             {
                 Backwards.ChBackwards();
-
             }
 
+            //If selection is equal to 3 then run Age Convert
+            if (userSel == 3)
+            {
+                AgeConvert.ChAgeConvert();
+            }
+
+            //If selection is equal to 4 then run TempConvert
+            if (userSel == 4)
+            {
+                TempConvert.ChTempConvert();
+            }
+
+            //If selection is equal to 5 then run BigBlueFish
+            if (userSel == 5)
+            {
+                BigBlueFish.ChBigBlueFish();
+            }
+
+            //If selection is equal to 0 then return to Menu
             if (userSel == 0)
             {
                 Environment.Exit(0);
-
             }
 
-
         }
-
-       
-
     }
 }
